402f4353da921a96efb3a0e579f03cc5fc42e9d2
FranzyPants <franzypants@icloud.com>
2013-11-10 22:53:45 -0800

    C++ Practice 1

---
+++ c++/A1-code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int apple;
+
+  apple = 8;
+
+  cout << "There are 'apple' apples\n"
+       << "Now there are 'apple-3' apples\n";
+
+  return 0;
+}

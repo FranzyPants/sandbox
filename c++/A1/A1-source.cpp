536a8cdee0542666faec23134312faaef5a6bc35
FranzyPants <franzypants@icloud.com>
2013-11-12 19:49:12 -0800

    Replaced missing source code and executable

---
+++ c++/A1/A1-source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int apple=8;
+
+  cout << "There are "<<apple<<" apples\n"
+       << "Now there are "<<apple-3<<" apples\n";
+
+  return 0;
+}

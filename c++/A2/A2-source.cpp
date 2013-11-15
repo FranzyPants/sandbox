1212581026505def58555a6531dfc722d9b29c2a
FranzyPants <franzypants@icloud.com>
2013-11-14 19:42:31 -0800

    I THINK I AM LEARNING

---
+++ c++/A2/A2-source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int apple=5;
+  int orange=2;
+
+  cout << "Jack has "<<apple<<" apples.\n"
+       << "Jill has "<<orange<<" oranges.\n"
+       << "Tom has "<<apple+orange<<" bananas.\n"
+       << "Betty has "<<(apple+orange)*2<<" pears.\n";
+
+  return 0;
+}

bb82f501a557f5d7e4a0b1e64bd96ed1e091959b
FranzyPants <franzypants@icloud.com>
2013-11-14 21:39:32 -0800

    A2_2 source and compiled files

---
+++ c++/A2/A2_2-source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a=5;
+  int o=2;
+  int b=7;
+  int p=14;
+
+  cout << "Jack has "<<a<<" apples.\n"
+       << "Jill has "<<o<<" oranges.\n"
+       << "Tom has "<<b<<" bananas.\n"
+       << "Betty has "<<p<<" pears.\n";
+
+  return 0;
+}

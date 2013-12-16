0a8443a42ef31fea5b578ddebe3331f56c682238
FranzyPants <franzypants@icloud.com>
2013-12-16 11:12:52 -0800

    Used math to declare variable

---
+++ c++/A2/A2_3-source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a=5;
+  int o=2;
+  int b=a+o;
+  int p=b*2;
+
+  cout << "Jack has "<<a<<" apples.\n"
+       << "Jill has "<<o<<" oranges.\n"
+       << "Tom has "<<b<<" bananas.\n"
+       << "Betty has "<<p<<" pears.\n";
+
+  return 0;
+}

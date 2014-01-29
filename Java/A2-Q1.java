f271da2ab6f44b4119c121363d29d06137c4befc
FranzyPants <franzypants@icloud.com>
2014-01-28 21:54:37 -0800

    Added Java directory for CS301J

---
+++ Java/A2-Q1.java
@@ -0,0 +1,16 @@
+//In this program, I am trying to get the titles to be left aligned and the
+//values to be right aligned. When I run this program as is, the alignment
+//is not correct. I used "\t" to try right aligning values.
+
+public class PersonalInfo
+{
+    public PersonalInfo()
+    {
+        System.out.println("Name:\tMatthew Stanwick");
+        System.out.println("Email:\tstanw002@cougars.csusm.edu");
+        System.out.println("Student ID:\t001177485");
+        System.out.println("Instructor Name:\tDr. Ahmad R. Hadaegh");
+        System.out.println("Course:\tComputer Mastery - CS301");
+        System.out.println("Assignment #:\tA2-Q1");
+    }
+}
